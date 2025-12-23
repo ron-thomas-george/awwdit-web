d295ac4a89db0c301c043f884c35fe3c1d04d409
ron-thomas-george <ronthomasgeorge@gmail.com>
2025-12-23 17:39:04 +0530

    feat: update configuration for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+  },
 })
